f365aa577dd656a7c89794c47b2e115544c3f094
maishixu <1269735668@qq.com>
2025-02-03 16:19:46 +0800

    初始化项目、Button组件

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<script setup lang="ts">
+import Button from './components/Button/Button.vue';
+</script>
+
+<template>
+  <main>
+    <Button ref="buttonRef">Test Button</Button>
+    <Button plain>Plain Button</Button>
+    <Button round>Round Button</Button>
+    <Button circle>MX</Button>
+    <Button disabled>Disabled Button</Button><br /><br />
+    <Button type="primary">Primary</Button>
+    <Button type="success">Success</Button>
+    <Button type="info">Info</Button>
+    <Button type="warning">Warning</Button>
+    <Button type="danger">Danger</Button><br /><br />
+    <Button type="primary" plain>Primary</Button>
+    <Button type="success" plain>Success</Button>
+    <Button type="info" plain>Info</Button>
+    <Button type="warning" plain>Warning</Button>
+    <Button type="danger" plain>Danger</Button><br /><br />
+    <Button size="large">Large</Button>
+    <Button size="small">Small</Button><br /><br />
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
---
+++ src/components/Button/Button.vue
@@ -0,0 +1,39 @@
+<template>
+  <button
+    ref="_ref"
+    class="mx-button"
+    :class="{
+      [`mx-button--${type}`]: type,
+      [`mx-button--${size}`]: size,
+      'is-plain': plain,
+      'is-round': round,
+      'is-circle': circle,
+      'is-disabled': disabled,
+      'is-loading': loading,
+    }"
+    :disabled="disabled"
+    :autofocus="autofocus"
+    :type="nativeType"
+  >
+    <span>
+      <slot />
+    </span>
+  </button>
+</template>
+<script setup lang="ts">
+import { ref } from 'vue'
+import type { ButtonProps } from './types'
+
+withDefaults(defineProps<ButtonProps>(), {
+  nativeType: 'button',
+})
+
+const _ref = ref<HTMLButtonElement>()
+
+defineExpose({
+  ref: _ref,
+})
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
